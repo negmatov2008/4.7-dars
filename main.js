960e56b4577457277a4e8597ab8b5c7726fdf383
nodirbek984 <nodirbeknegmatov2008@gmail.com>
2023-11-23 21:49:56 +0500

    tugadi

---
+++ main.js
@@ -0,0 +1,124 @@
+//misol15
+// let son = +prompt("sonni kiriting")
+
+// if (son <= 0){
+//     alert("true")
+// }else{
+//     alert("False")
+// }
+
+//misol16
+// let burchak = +prompt("Nechi burchak")
+//  alert(`${(burchak -2) *180} javobi`)
+
+//misol17
+// let a = +prompt("2 achkoli gollar sonnini kiriting");
+// let b = +prompt("3 achkoli gollar sonnini kiriting");
+
+// function sum(x, y) {
+//   let res = x * 2 + y * 3;
+//   return res;
+// }
+
+// let nat = sum(a, b);
+// alert(nat);
+
+//misol18
+// let a = prompt("Soz kiriting");
+
+// function sum(x) {
+//   let res = `${x} edabit`;
+//   return res;
+// }
+
+// let nat = sum(a);
+// alert(nat);
+
+//misol19
+// let son1 = +prompt("Birinchi sonni kiriting");
+// let son2 = +prompt("ikkinchi sonni kiriting");
+// function sum(x, y) {
+//   if (x + y > 100) {
+//     let res1 = alert("false");
+//     return res1;
+//   } else {
+//     let res2 = alert("true");
+//     return res2;
+//   }
+// }
+// let nat = sum(son1, son2);
+// alert(nat);
+
+//misol21
+// let tovuq = +prompt("tovuqlar sonnini kiriting");
+// let qoy = +prompt("qoylar sonnini kiriting");
+// let sigir = +prompt("sigirlar sonni kiriting");
+// function sum(x, y, z) {
+//   let res = x * 2 + y * 4 + z * 4;
+//   return res;
+// }
+// let nat = sum(tovuq, qoy, sigir);
+// alert(nat);
+
+//misol23
+// let a = +prompt("1-sonni kiriting");
+// let b = +prompt("2-sonni kiriting");
+// function sum(x, y) {
+//   if (x == y) {
+//     return alert("Rost");
+//   } else {
+//     return alert("Yolgon");
+//   }
+// }
+// let nat = sum(a, b);
+// alert(nat);
+
+//misol24
+// let win = +prompt("Nechi marta yutganingizni kiriting");
+// let equal = +prompt("Durrang oyinlar sonnini kirirting");
+// let feild = +prompt("Maglubiyatlar sonnini kiriting");
+// function sum(x, y, z) {
+//   let res = x * 3 + y * 1 + z * 0;
+//   return res;
+// }
+// let nat = sum(win, equal, feild);
+
+//misol25
+// let h = +prompt("Nechi soat");
+// let min = +prompt("Nechi minut");
+// function sum(x, y) {
+//   let res = x * 3600 + y * 60;
+//   return res;
+// }
+// let nat = sum(h, min);
+// alert(nat);
+
+//misol27
+// let a = +prompt("Biror nima kiriting");
+// let b = String(prompt("Biror narsa kiriting"));
+// function sum(x, y) {
+//   if (String(x) == String(y)) {
+//     return alert("true");
+//   } else if (+x == +y) {
+//     return alert("true");
+//   } else {
+//     return alert("false");
+//   }
+// }
+// let nat = sum(a, b);
+// alert(nat);
+
+//misol28
+// let a = prompt("Biror nima kiriting");
+// const sum = (x) => alert(x);
+// let nat = sum(a);
+// LockManager(nat);
+
+//misol31
+// let a = String(prompt("Matematika ifoda kirirting"));
+// number(a);
+// const sum = (x) => alert(number(x));
+// let nat = sum(a);
+// alert(nat);
+
+//misol
